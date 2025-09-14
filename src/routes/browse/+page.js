00b27a887812dfc8c72f5a79b287a0f7e4988a12
paulwuertz <paulwuertz@posteo.de>
2025-09-14 11:42:48 +0200

    add entry browse subpage to select a version --- encoded via link, need to rethink link vs js selected value...

---
+++ src/routes/browse/+page.js
@@ -0,0 +1,19 @@
+import { goto } from '$app/navigation';
+import { symbols } from '../symbols.svelte.js';
+
+export const trailingSlash = 'always';
+export const prerender = false;
+export const ssr = false;
+export const csr = true;
+
+export async function load({ url, parent }) {
+	// load elf data
+	let componentData = await parent();
+	console.log('sym', symbols);
+
+	if (!symbols.elfDataProvided) {
+		goto('/');
+		return;
+	}
+	return componentData;
+}
---
+++ src/routes/browse/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { onMount } from 'svelte';
+	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+	import { writable } from 'svelte/store';
+	import { Card, Button, Col, CardHeader, Container, Input, Row, CardBody, CardText, CardTitle } from '@sveltestrap/sveltestrap';
+	// ui stuff
+	import { DataTable } from '@careswitch/svelte-data-table';
+
+	import * as echarts from 'echarts';
+	import { symbols } from '../symbols.svelte.js';
+	import * as helpers from '../helpers.js';
+
+	let { data } = $props();
+	let files = $state();
+	let versions = $derived(Object.keys(symbols.symbols));
+	let selected_symbols = $state({});
+</script>
+
+<hr>
+
+<div class="container" id="content">
+    <h3>Select a version to browse its symbols:</h3>
+
+    <Row cols={{ lg: 3, md: 2, sm: 1 }}>
+        {#each versions as version}
+        <div class="pb-3 px-3">
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>{version}</CardTitle>
+                </CardHeader>
+                <CardBody>
+                    <CardText>
+                        <!-- TODO add source link symbol json -->
+                        Buildtime: {symbols.symbols[version].timestamp}
+                    </CardText>
+                    <Button href={version}>
+                        Browse
+                    </Button>
+                </CardBody>
+            </Card>
+        </div>
+        {/each}
+	</Row>
+</div>
