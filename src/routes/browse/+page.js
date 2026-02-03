a78c08afbd936247f975eb4df2ee02a7dc70d0b3
paulwuertz <paulwuertz@posteo.de>
2026-02-03 10:32:12 +0100

    go directly to browse page if only one version provided

--- src/routes/browse/+page.js
+++ src/routes/browse/+page.js
@@ -10,9 +10,15 @@ export const csr = true;
 export async function load({ url, parent }) {
 	// load elf data
 	let componentData = await parent();
-	console.log('browse', symbols);
-
-	if (!symbols.elfDataProvided) {
+	
+	let versions = Object.keys(symbols.symbols)
+	console.log('browse', symbols, versions, versions.length);
+	if (versions.length == 1) {
+		let selVers = versions[0]
+		symbols.elfDataProvided = true
+		symbols.selected_version = selVers
+		goto(base + '/browse/' + selVers + "/");
+	}else if (!symbols.elfDataProvided) {
 		goto(base + '/');
 		return;
 	}
