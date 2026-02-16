dc05176096fdf6f1be146356ab69abaa55abb4a8
paulwuertz <paulwuertz@posteo.de>
2026-02-16 22:13:24 +0100

    rm missing route options - fix build

--- src/routes/wip/+page.js
+++ src/routes/wip/+page.js
@@ -2,11 +2,6 @@ import { goto } from '$app/navigation';
 import { symbols } from '../symbols.svelte.js';
 import { base } from '$app/paths';
 
-export const trailingSlash = 'always';
-export const prerender = false;
-export const ssr = false;
-export const csr = true;
-
 export async function load({ url, parent }) {
 	// load elf data
 	let componentData = await parent();
