17c2cfc88b93e170b6e73f64e961472929c297f8
paulwuertz <paulwuertz@posteo.de>
2026-06-03 23:23:41 +0200

    delte config lines, list fw versions and functions to add new ones

--- src/components/SettingsEditor.svelte
+++ src/components/SettingsEditor.svelte
@@ -21,6 +21,7 @@
 	import { symbols } from '../routes/symbols.svelte.js';
 	import * as helpers from '../routes/helpers.js';
 
+
 	const { settings } = $props();
 
     let thread_stack_size = (thread) => {
@@ -32,15 +33,28 @@
             return 0 // TODO error
         }
     }
-
-    let threads = settings.threads || []
-    let dynamic_calls = settings.dynamic_calls || []
-	let versions = $derived(Object.keys(symbols.symbols));
+    let setting = $derived(settings)
+    let threads = $derived(setting.threads || [])
+    let dynamic_calls = $derived(setting.dynamic_calls || [])
+    let versions = $derived(Object.keys(symbols.symbols));
+    let version_name = $derived(versions[0]);
+	let version = $derived(symbols.symbols[version_name] || {});
+	let functions = $derived(version.functions || []);
+	let variables = $derived(version.variables || []);
 </script>
 
 <div class="container" id="content">
 	<h3>pexplorer setting:</h3>
-	<h4>threads:</h4>
+
+    Select symbols from:
+
+    <Input type="select">
+        {#each versions as option}
+        <option>{option}</option>
+        {/each}
+    </Input>
+
+	<h4>Threads:</h4>
 
     <Table bordered>
     <thead>
@@ -52,13 +66,13 @@
         </tr>
     </thead>
     <tbody>
-        {#each threads as thread (thread.thread_entry_name)}
+        {#each threads as thread, index (thread.thread_entry_name+index)}
         <tr>
             <td>{thread.thread_entry_name}</td>
             <td>{thread.stack_variable_name}</td>
             <td>{thread_stack_size(thread)}</td>
             <td>
-                <Button size="md" outline color="danger">
+                <Button onclick={() => {threads = threads.filter((v, i) => i!=index)}} size="md" outline color="danger">
                     <svg height="32" width="29" viewBox="0 0 1000 875"><path d="M0 281.296l0 -68.355q1.953 -37.107 29.295 -62.496t64.449 -25.389l93.744 0l0 -31.248q0 -39.06 27.342 -66.402t66.402 -27.342l312.48 0q39.06 0 66.402 27.342t27.342 66.402l0 31.248l93.744 0q37.107 0 64.449 25.389t29.295 62.496l0 68.355q0 25.389 -18.553 43.943t-43.943 18.553l0 531.216q0 52.731 -36.13 88.862t-88.862 36.13l-499.968 0q-52.731 0 -88.862 -36.13t-36.13 -88.862l0 -531.216q-25.389 0 -43.943 -18.553t-18.553 -43.943zm62.496 0l749.952 0l0 -62.496q0 -13.671 -8.789 -22.46t-22.46 -8.789l-687.456 0q-13.671 0 -22.46 8.789t-8.789 22.46l0 62.496zm62.496 593.712q0 25.389 18.553 43.943t43.943 18.553l499.968 0q25.389 0 43.943 -18.553t18.553 -43.943l0 -531.216l-624.96 0l0 531.216zm62.496 -31.248l0 -406.224q0 -13.671 8.789 -22.46t22.46 -8.789l62.496 0q13.671 0 22.46 8.789t8.789 22.46l0 406.224q0 13.671 -8.789 22.46t-22.46 8.789l-62.496 0q-13.671 0 -22.46 -8.789t-8.789 -22.46zm31.248 0l62.496 0l0 -406.224l-62.496 0l0 406.224zm31.248 -718.704l374.976 0l0 -31.248q0 -13.671 -8.789 -22.46t-22.46 -8.789l-312.48 0q-13.671 0 -22.46 8.789t-8.789 22.46l0 31.248zm124.992 718.704l0 -406.224q0 -13.671 8.789 -22.46t22.46 -8.789l62.496 0q13.671 0 22.46 8.789t8.789 22.46l0 406.224q0 13.671 -8.789 22.46t-22.46 8.789l-62.496 0q-13.671 0 -22.46 -8.789t-8.789 -22.46zm31.248 0l62.496 0l0 -406.224l-62.496 0l0 406.224zm156.24 0l0 -406.224q0 -13.671 8.789 -22.46t22.46 -8.789l62.496 0q13.671 0 22.46 8.789t8.789 22.46l0 406.224q0 13.671 -8.789 22.46t-22.46 8.789l-62.496 0q-13.671 0 -22.46 -8.789t-8.789 -22.46zm31.248 0l62.496 0l0 -406.224l-62.496 0l0 406.224z"/></svg>
                 </Button>
             </td>
@@ -69,16 +83,16 @@
             <td>
                 <Input type="select">
                     <option></option>
-                    {#each [1, 2, 3, 4, 5] as option}
-                    <option>{option}</option>
+                    {#each functions as f}
+                    <option>{f.name}</option>
                     {/each}
                 </Input>
             </td>
             <td>
                 <Input type="select">
                     <option></option>
-                    {#each [1, 2, 3, 4, 5] as option}
-                    <option>{option}</option>
+                    {#each variables as v}
+                    <option>{v.name}</option>
                     {/each}
                 </Input>
             </td>
@@ -109,7 +123,7 @@
             <td>{dynamic_call.call_from}</td>
             <td>{dynamic_call.call_to}</td>
             <td>
-                <Button size="md" outline color="danger">
+                <Button onclick={() => {dynamic_calls = dynamic_calls.filter((v, i) => i!=index)}} size="md" outline color="danger">
                     <svg height="32" width="29" viewBox="0 0 1000 875"><path d="M0 281.296l0 -68.355q1.953 -37.107 29.295 -62.496t64.449 -25.389l93.744 0l0 -31.248q0 -39.06 27.342 -66.402t66.402 -27.342l312.48 0q39.06 0 66.402 27.342t27.342 66.402l0 31.248l93.744 0q37.107 0 64.449 25.389t29.295 62.496l0 68.355q0 25.389 -18.553 43.943t-43.943 18.553l0 531.216q0 52.731 -36.13 88.862t-88.862 36.13l-499.968 0q-52.731 0 -88.862 -36.13t-36.13 -88.862l0 -531.216q-25.389 0 -43.943 -18.553t-18.553 -43.943zm62.496 0l749.952 0l0 -62.496q0 -13.671 -8.789 -22.46t-22.46 -8.789l-687.456 0q-13.671 0 -22.46 8.789t-8.789 22.46l0 62.496zm62.496 593.712q0 25.389 18.553 43.943t43.943 18.553l499.968 0q25.389 0 43.943 -18.553t18.553 -43.943l0 -531.216l-624.96 0l0 531.216zm62.496 -31.248l0 -406.224q0 -13.671 8.789 -22.46t22.46 -8.789l62.496 0q13.671 0 22.46 8.789t8.789 22.46l0 406.224q0 13.671 -8.789 22.46t-22.46 8.789l-62.496 0q-13.671 0 -22.46 -8.789t-8.789 -22.46zm31.248 0l62.496 0l0 -406.224l-62.496 0l0 406.224zm31.248 -718.704l374.976 0l0 -31.248q0 -13.671 -8.789 -22.46t-22.46 -8.789l-312.48 0q-13.671 0 -22.46 8.789t-8.789 22.46l0 31.248zm124.992 718.704l0 -406.224q0 -13.671 8.789 -22.46t22.46 -8.789l62.496 0q13.671 0 22.46 8.789t8.789 22.46l0 406.224q0 13.671 -8.789 22.46t-22.46 8.789l-62.496 0q-13.671 0 -22.46 -8.789t-8.789 -22.46zm31.248 0l62.496 0l0 -406.224l-62.496 0l0 406.224zm156.24 0l0 -406.224q0 -13.671 8.789 -22.46t22.46 -8.789l62.496 0q13.671 0 22.46 8.789t8.789 22.46l0 406.224q0 13.671 -8.789 22.46t-22.46 8.789l-62.496 0q-13.671 0 -22.46 -8.789t-8.789 -22.46zm31.248 0l62.496 0l0 -406.224l-62.496 0l0 406.224z"/></svg>
                 </Button>
             </td>
@@ -120,16 +134,16 @@
             <td>
                 <Input type="select">
                     <option></option>
-                    {#each [1, 2, 3, 4, 5] as option}
-                    <option>{option}</option>
+                    {#each functions as f}
+                    <option>{f.name}</option>
                     {/each}
                 </Input>
             </td>
             <td>
                 <Input type="select">
                     <option></option>
-                    {#each [1, 2, 3, 4, 5] as option}
-                    <option>{option}</option>
+                    {#each functions as f}
+                    <option>{f.name}</option>
                     {/each}
                 </Input>
             </td>
